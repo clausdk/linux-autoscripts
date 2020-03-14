c77bbf2555fc8cbb1168972fed52f53730b9d8ab
clausdk <cba@1it.dk>
2020-03-15 00:01:16 +0100

    Update fix-proxmox-quorum-multicast.sh

--- fix-proxmox-quorum-multicast.sh
+++ fix-proxmox-quorum-multicast.sh
@@ -4,5 +4,5 @@ echo "Start: Checking that multicast is set properly"
 eval 'echo 0 >/sys/class/net/vmbr0/bridge/multicast_snooping'
 echo "Done"
 echo "Start: Running the cluster restart script"
-eval 'systemctl start pve-cluster'
+systemctl start pve-cluster
 echo "Done"
