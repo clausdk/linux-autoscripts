1bb0bd704f84789dc0660828bfd6075deff12b15
clausdk <cba@1it.dk>
2020-03-15 00:02:16 +0100

    Delete fix-proxmox-quorum-multicast.sh

--- fix-proxmox-quorum-multicast.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "This is the automated restart cluster script, you will need to run this on the other nodes."
-echo "Start: Checking that multicast is set properly"
-eval 'echo 0 >/sys/class/net/vmbr0/bridge/multicast_snooping'
-echo "Done"
-echo "Start: Running the cluster restart script"
-systemctl start pve-cluster
-echo "Done"
