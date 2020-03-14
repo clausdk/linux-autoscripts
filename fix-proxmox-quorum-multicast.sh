8c1e2f4d72ee663cd67aeaf8556c26e1f45e7c16
clausdk <cba@1it.dk>
2020-03-14 23:58:31 +0100

    Create fix-proxmox-quorum-multicast.sh

---
+++ fix-proxmox-quorum-multicast.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "This is the automated restart cluster script, you will need to run this on the other nodes."
+echo "Start: Checking that multicast is set properly"
+eval 'echo 0 >/sys/class/net/vmbr0/bridge/multicast_snooping'
+echo "Done"
+echo "Start: Running the cluster restart script"
+eval '/etc/init.d/pve-cluster restart'
+echo "Done"
+echo "Start: Running the pvedaemon restart script"
+eval '/etc/init.d/pve-cluster restart'
+echo "Done"
+echo "Start: Running the pve-manager restart script this typically takes longer to run "
+eval '/etc/init.d/pve-cluster restart'
+echo "Done"
+echo "this looks like it ran properly, run this on the other nodes, you should regain quorum"
